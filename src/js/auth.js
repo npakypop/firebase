4151907aba663befe9c45ac0c8d275d85d386bbc
npakypop <leaterood@gmail.com>
2022-12-03 09:13:21 +0200

    add listener on auth status

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,25 +1,43 @@
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { getAuth, signOut } from "firebase/auth";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+// listen for auth status changes
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+    console.log("onAuthStateChanged ~ user", user);
+    if (user) {
+      console.log('User logged in: ', user);
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    console.log('User logged out');
+  }
+});
 
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', onSignup);
 
 function onSignup(event) {
     event.preventDefault();
-    //get user info
+    // Get user info
     const email = signupForm['signup-email'].value;
     const password = signupForm['signup-password'].value;
     console.log(email, password);
 
-    //signup user
+    // Signup user
     const auth = getAuth();
     createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
         console.log("user", user);
-        //close modal
+
+        // Close modal
         const modal = document.querySelector('#modal-signup')
         M.Modal.getInstance(modal).close();
         signupForm.reset();
@@ -30,23 +48,23 @@ function onSignup(event) {
     });
 }
 
- //LOGOUT
+ // LOGOUT
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', onLogoutBtn);
 
 function onLogoutBtn(event) { 
     event.preventDefault();
     const auth = getAuth();
-    signOut(auth).then(() => console.log('sign out'))
+    signOut(auth).then();
 }
 
-//continue lesson 7 LOGIN ==>
+// Continue lesson 7 LOGIN ==>
 const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', onLoginBtn);
 
 function onLoginBtn(event) {
     event.preventDefault()
-    //get user info
+    // Get user info
     const email = loginForm['login-email'].value;
     const password = loginForm['login-password'].value;
     
@@ -55,8 +73,9 @@ function onLoginBtn(event) {
       .then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
-        console.log(user);
-        //close modal
+        // console.log(user);
+          
+        // Close modal
         const modal = document.querySelector('#modal-login');
         M.Modal.getInstance(modal).close();
         loginForm.reset();
